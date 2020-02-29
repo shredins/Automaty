9650ea869202d1b569bd3f42d50e9e595365a8d6
Tomasz Korab <tomasz.korab95@gmail.com>
2020-02-29 18:59:29 +0100

    Screen setup

--- Automaty/AppDelegate.swift
+++ Automaty/AppDelegate.swift
@@ -11,8 +11,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let window = UIWindow(frame: UIScreen.main.bounds)
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
         return true
     }
 
