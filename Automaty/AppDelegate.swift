cb64b354b210bc3129d33b59784de80a693d13a3
Tomasz Korab <tomasz.korab95@gmail.com>
2020-03-01 20:16:44 +0100

    Secrets configuration

--- Automaty/AppDelegate.swift
+++ Automaty/AppDelegate.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Keys
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let keys = AutomatyKeys()
     let window = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
